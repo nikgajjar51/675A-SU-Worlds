9493afd2a358c91c6d7986d25f7d846eeafafcec
Nikhil Gajjar <nikgajjar51@gmail.com>
2023-03-13 22:20:16 -0400

    iDummy

--- include/675A/constants.hpp
+++ include/675A/constants.hpp
@@ -5,9 +5,9 @@ extern const int drive_speed, turn_speed, swerve_speed;
 extern const double low_speed_multiplier, normal_speed_multiplier,
     high_speed_multiplier, balls_to_the_walls;
 
-void chassis_default_constants(void *);
-void chassis_exit_condition_defaults(void *);
-void chassis_modified_exit_condition(void *);
+void chassis_default_constants();
+void chassis_exit_condition_defaults();
+void chassis_modified_exit_condition();
 // Flywheel Constants
 #define flywheel_smooth_size 3
 #define flywheel_integral_smoothing 2
