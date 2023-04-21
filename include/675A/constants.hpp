12f69535ac89a002e0a482bc2173b4d6074b1056
Nikhil Gajjar <nikgajjar51@gmail.com>
2023-04-20 21:49:43 -0400

    remove old fly wheel things

--- include/675A/constants.hpp
+++ include/675A/constants.hpp
@@ -14,13 +14,6 @@ void chassis_default_constants();
 void chassis_exit_condition_defaults();
 void chassis_modified_exit_condition();
 
-// Flywheel Constants
-#define flywheel_smooth_size 3
-#define flywheel_integral_smoothing 2
-extern double flywheel_speeds[flywheel_smooth_size], flywheel_kP, flywheel_kI,
-    flywheel_integral, flywheel_error, flywheel_speed,
-    flywheel_current_velocity;
-
 // Feed Forward Constants
 extern const double kP, kI, kD, kF;
 extern double error, lastError, derivative, integral, currentVelocity, power,
